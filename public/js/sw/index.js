640ea1de128ab809cdd4e106478ea57dd303033a
webdevjlprojects <webdev.jl@gmail.com>
2017-12-19 12:26:44 +0000

    Lesson 3 Quiz 11

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,9 @@
 self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+      new Response('<h1 class="a-winner-is-me">Sending a response from the service worker</h1>', {
+          headers: {
+              'Content-Type': 'text/html'
+          },
+      })
+  );
 });
