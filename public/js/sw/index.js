4be6715e79e8acfe0051be8e9d6c2ba6c412bbfa
webdevjlprojects <webdev.jl@gmail.com>
2017-12-20 12:46:52 +0000

    Lesson 3 - quiz 17

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,15 +1,18 @@
 self.addEventListener('install', function(event) {
-  var urlsToCache = [
-    '/',
-    'js/main.js',
-    'css/main.css',
-    'imgs/icon.png',
-    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-  ];
-
   event.waitUntil(
     // TODO: open a cache named 'wittr-static-v1'
+    caches.open('wittr-static-v1').then(function(cache) {
+      var urlsToCache = [
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ];
+      
+      return cache.addAll(urlsToCache);
+    })
     // Add cache the urls from urlsToCache
   );
 });
