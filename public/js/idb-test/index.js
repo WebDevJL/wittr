47c4deebbd4a8539c4af04b0cbd3cbaa28292e2c
webdevjlprojects <webdev.jl@gmail.com>
2017-12-29 12:28:57 +0000

    Quiz 4.3

--- public/js/idb-test/index.js
+++ public/js/idb-test/index.js
@@ -1,8 +1,19 @@
 import idb from 'idb';
 
-var dbPromise = idb.open('test-db', 1, function(upgradeDb) {
-  var keyValStore = upgradeDb.createObjectStore('keyval');
-  keyValStore.put("world", "hello");
+var dbPromise = idb.open('test-db', 3, function(upgradeDb) {
+  switch (upgradeDb.oldVersion) {
+    case 0:
+      var keyValStore = upgradeDb.createObjectStore('keyval');
+      keyValStore.put("world", "hello");
+      //break;<-- we don't need it 
+    case 1:
+      upgradeDb.createObjectStore('people', { keyPath: 'name' });
+    case 2:
+      var peopleStore = upgradeDb.transaction.objectStore('people');
+      peopleStore.createIndex('animal', 'favoriteAnimal');
+      //upgradeDb.createObjectStore('people', { keyPath: 'name' });
+  }
+
 });
 
 // read "hello" in "keyval"
@@ -28,4 +39,55 @@ dbPromise.then(function(db) {
   // TODO: in the keyval store, set
   // "favoriteAnimal" to your favourite animal
   // eg "cat" or "dog"
+  var tx = db.transaction('keyval', 'readwrite');
+  var keyValStore = tx.objectStore('keyval');
+  keyValStore.put('cat', 'favoriteAnimal');
+  return tx.complete;
+}).then(function() {
+  console.log('Added favoriteAnimal:cat to keyval');
+});
+
+dbPromise.then(function(db) {
+  var tx = db.transaction('people', 'readwrite');
+  var peopleStore = tx.objectStore('people');
+
+  peopleStore.put({
+    name: 'Jérémie Litzler',
+    age: 30,
+    favoriteAnimal: 'Cheetath'
+  });
+  peopleStore.put({
+    name: 'Aurélie Litzler',
+    age: 31,
+    favoriteAnimal: 'Cat'
+  });
+  peopleStore.put({
+    name: 'Léyla Litzler',
+    age: 3,
+    favoriteAnimal: 'Cat'
+  });
+  peopleStore.put({
+    name: 'Alyssia Litzler',
+    age: 3,
+    favoriteAnimal: 'Sheep'
+  });
+  peopleStore.put({
+    name: 'Nicolas Lécluse',
+    age: 28,
+    favoriteAnimal: 'Dog'
+  });
+
+  return tx.complete;
+}).then(function() {
+  console.log('Person added!');
+});
+
+dbPromise.then(function(db) {
+  var tx = db.transaction('people');
+  var peopleStore = tx.objectStore('people');
+  var animalIndex = peopleStore.index('animal');
+
+  return animalIndex.getAll();
+}).then(function(people) {
+  console.log(people);
 });
